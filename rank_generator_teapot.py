4b04a598056dbeb8021564e585816bdad8123572
shyammarjit <shyam.marjit@iiitg.ac.in>
2024-02-02 04:58:00 -0500

    extracted more images

---
+++ rank_generator_teapot.py
@@ -0,0 +1,130 @@
+import os
+import sys
+import io
+import requests
+import PIL
+import torch
+from torch import autocast
+import random
+from tqdm import tqdm
+from transformers import CLIPTextModel
+from diffusers import (
+    AutoencoderKL,
+    LMSDiscreteScheduler,
+    DDIMScheduler,
+    PNDMScheduler,
+    DPMSolverMultistepScheduler,
+    EulerDiscreteScheduler,
+    EulerAncestralDiscreteScheduler,
+    StableDiffusionPipeline
+)
+from PIL import Image
+from svdiff_pytorch import load_unet_for_svdiff, load_text_encoder_for_svdiff, SCHEDULER_MAPPING, image_grid
+MODEL_NAME="runwayml/stable-diffusion-v1-5"
+
+#====================================================================
+## Please change this one only.
+spectral_shifts_ckpt = "/home/shyam/svdiff_output_rank/teapot"
+output_folder_path = "./output_rank/teapot"
+# seeds = [1, 2, 3]
+seeds=1
+prompts = ['A sks teapot on top of a dirt road', 'A sks teapot floating on top of water']
+#====================================================================
+
+
+def load_text_encoder(pretrained_model_name_or_path, spectral_shifts_ckpt, device, fp16=False):
+    if os.path.isdir(spectral_shifts_ckpt):
+        spectral_shifts_ckpt = os.path.join(spectral_shifts_ckpt, "spectral_shifts_te.safetensors")
+    elif not os.path.exists(spectral_shifts_ckpt):
+        # download from hub
+        hf_hub_kwargs = {} if hf_hub_kwargs is None else hf_hub_kwargs
+        try:
+            spectral_shifts_ckpt = huggingface_hub.hf_hub_download(spectral_shifts_ckpt, filename="spectral_shifts_te.safetensors", **hf_hub_kwargs)
+        except huggingface_hub.utils.EntryNotFoundError:
+            return CLIPTextModel.from_pretrained(pretrained_model_name_or_path, subfolder="text_encoder", torch_dtype=torch.float16 if fp16 else None).to(device)
+    if not os.path.exists(spectral_shifts_ckpt):
+            return CLIPTextModel.from_pretrained(pretrained_model_name_or_path, subfolder="text_encoder", torch_dtype=torch.float16 if fp16 else None).to(device)
+    text_encoder = load_text_encoder_for_svdiff(
+        pretrained_model_name_or_path=pretrained_model_name_or_path,
+        spectral_shifts_ckpt=spectral_shifts_ckpt,
+        subfolder="text_encoder",
+    )
+    # first perform svd and cache
+    for module in text_encoder.modules():
+        if hasattr(module, "perform_svd"):
+            module.perform_svd()
+    if fp16:
+        text_encoder = text_encoder.to(device, dtype=torch.float16)
+    return text_encoder
+
+scheduler_type = "dpm_solver++" #@param ["ddim", "plms", "lms", "euler", "euler_ancestral", "dpm_solver++"]
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+unet = load_unet_for_svdiff(MODEL_NAME, spectral_shifts_ckpt=spectral_shifts_ckpt, subfolder="unet")
+unet = unet.to(device)
+for module in unet.modules():
+  if hasattr(module, "perform_svd"):
+    module.perform_svd()
+
+unet = unet.to(device, dtype=torch.float16)
+text_encoder = load_text_encoder(
+    pretrained_model_name_or_path=MODEL_NAME,
+    spectral_shifts_ckpt=spectral_shifts_ckpt,
+    device=device,
+    fp16=True,
+)
+# load pipe
+pipe = StableDiffusionPipeline.from_pretrained(
+    MODEL_NAME,
+    unet=unet,
+    text_encoder=text_encoder,
+    vae=AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse"),
+    requires_safety_checker=False,
+    safety_checker=None,
+    feature_extractor=None,
+    scheduler=SCHEDULER_MAPPING[scheduler_type].from_pretrained(MODEL_NAME, subfolder="scheduler"),
+    torch_dtype=torch.float16
+)
+
+pipe = pipe.to(device)
+print("loaded pipeline")
+
+num_images_per_prompt = 2 # @param {type: "integer"}
+guidance_scale = 7.5 # @param {type: "number"}
+num_inference_steps = 25 # @param {type: "integer"}
+height = 512 # @param {type: "integer"}
+width = 512 # @param {type: "integer"}
+spectral_shifts_scale = 1.0 #@param {type: "number"}
+
+
+if pipe.unet.conv_out.scale != spectral_shifts_scale:
+  for module in pipe.unet.modules():
+    if hasattr(module, "set_scale"):
+      module.set_scale(scale=spectral_shifts_scale)
+  for module in pipe.text_encoder.modules():
+    if hasattr(module, "set_scale"):
+      module.set_scale(scale=spectral_shifts_scale)
+  print(f"Set spectral_shifts_scale to {spectral_shifts_scale}!")
+
+
+if not os.path.isdir(output_folder_path):
+    os.mkdir(output_folder_path)
+
+all_images = []
+for prompt in tqdm(prompts):
+    if type(seeds)==int:
+        seeds = [seeds]
+    for seed in seeds:
+        g_cuda = torch.Generator(device='cuda').manual_seed(seed)
+        print(f"{prompt}")
+        print(f"seed: {seed}")
+        with torch.autocast(device), torch.inference_mode():
+            images = pipe(
+                prompt,
+                num_inference_steps=num_inference_steps,
+                guidance_scale=guidance_scale,
+                height=height,
+                width=width,
+                generator=g_cuda
+            ).images
+        images[0].save(os.path.join(output_folder_path, f"{seed}. {prompt}.png"))
---
+++ run_rank_monster_toy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=6
+#SBATCH --mem-per-cpu=10G
+#SBATCH -C gmem24
+#SBATCH --job-name=monster_toy
+#SBATCH --output=outputs_svdiff/monster_toy.out
+
+source activate svdiff
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="/home/shyam/svdiff-pytorch/dataset_rank/monster_toy"
+export CLASS_DIR="/home/shyam/class_data_rank/monster_toy"
+export OUTPUT_DIR="/home/shyam/svdiff_output_rank/monster_toy"
+
+accelerate launch train_svdiff.py \
+    --pretrained_model_name_or_path=$MODEL_NAME  \
+    --instance_data_dir=$INSTANCE_DIR \
+    --class_data_dir=$CLASS_DIR \
+    --output_dir=$OUTPUT_DIR \
+    --with_prior_preservation --prior_loss_weight=1.0 \
+    --instance_prompt="photo of a sks toy" \
+    --class_prompt="photo of a toy" \
+    --resolution=512 \
+    --train_batch_size=1 \
+    --gradient_accumulation_steps=1 \
+    --learning_rate=1e-3 \
+    --learning_rate_1d=1e-6 \
+    --train_text_encoder \
+    --lr_scheduler="constant" \
+    --lr_warmup_steps=0 \
+    --num_class_images=200 \
+    --max_train_steps=1000 \
+    --checkpointing_steps=500 \
+    --use_8bit_adam \
+    --seed=42 \
+    --enable_xformers_memory_efficient_attention \
+    --gradient_checkpointing
